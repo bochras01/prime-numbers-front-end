985818ee83ea2bab935b6bb21d55382bc2b955e6
bochra ss <bochra.salahsalah@gmail.com>
2022-05-23 21:27:17 +0200

    add app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SearchResultComponent} from "./search-result/search-result.component";
+import {SearchHistoryComponent} from "./search-history/search-history.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchResultComponent },
+  { path: 'search-history', component: SearchHistoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
